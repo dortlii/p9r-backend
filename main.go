4d0991f311c325d8e1dab457003f84e731d70312
dortlii <22558338+dortlii@users.noreply.github.com>
2023-03-17 00:33:36 +0100

    feat: added route for namespace creation

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"github.com/dortlii/p9r-backend/controllers"
+	"github.com/dortlii/p9r-backend/initializers"
+	"github.com/gin-gonic/gin"
+)
 
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDb()
+}
+
+func main() {
+	r := gin.Default()
+
+	r.POST("/namespaces", controllers.NamespaceCreate)
+
+	r.Run()
 }
