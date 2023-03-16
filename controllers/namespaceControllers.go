52f70f2541e4142460a3b4adf91bb0221092e0c8
dortlii <22558338+dortlii@users.noreply.github.com>
2023-03-17 00:33:50 +0100

    feat: added controller NamespaceCreate

---
+++ controllers/namespaceControllers.go
@@ -0,0 +1,31 @@
+package controllers
+
+import (
+	"github.com/dortlii/p9r-backend/initializers"
+	"github.com/dortlii/p9r-backend/models"
+	"github.com/gin-gonic/gin"
+)
+
+func NamespaceCreate(c *gin.Context) {
+	// Get data off request body
+	var body struct {
+		Name string
+	}
+
+	c.Bind(&body)
+
+	// create the namespace
+	namespace := models.Namespace{Name: body.Name}
+
+	result := initializers.DB.Create(&namespace) // pass pointer of data to Create
+
+	if result.Error != nil {
+		c.Status(400)
+		return
+	}
+
+	// return it
+	c.JSON(200, gin.H{
+		"name": namespace,
+	})
+}
